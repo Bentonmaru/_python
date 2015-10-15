aa7a394c8ed3ddf3dc178fd8eeb0c0ed4e3c2823
Bentonmaru <bentonmaru@hotmail.com>
2015-10-15 13:52:46 -0500

    Changes made to reader.py

--- reddit_reader/reader.py
+++ reddit_reader/reader.py
@@ -1,16 +1,17 @@
 import praw
 
-subreddit = input("Please enter a subreddit to continue ")
+subreddit = input("Please enter a subreddit to continue: ")
 
 def main(submission):
 	r = praw.Reddit(user_agent='reddit_reader')
 	subname = [x.strip() for x in submission.split('/')]
 	submissions = r.get_subreddit(subname[-1]).get_hot(limit=30)
-	submission_form = "{}) {} : {} <{}>"
-	count = enumerate()
+	submission_form =  "{}) {} : {} <{}>"
 	print("Top 30 Posts from /r/" + subname[-1])
+	count = 1
 	for sub in submissions:
 		print(submission_form.format(count, sub.ups, sub.title, sub.url))
-		
+		count += 1
+	
 
 main(subreddit)
