4cb9db85848b5284f0377b9c3fbbc9b96c9ead17
Bentonmaru <bentonmaru@hotmail.com>
2015-10-19 12:28:15 -0500

    Added used_vehicle project

---
+++ used_vehicles/vehicle.py
@@ -0,0 +1,23 @@
+vehicle = raw_input("Please enter your brand, model, and year(e.g 1993) ")
+
+def parse_string(vehicle):
+	brand = vehicle.split()
+
+
+class Vehicle(object):
+	def __init__(self, brand, model, year):
+		self.brand = brand
+		self.model = model
+		self.year = year
+
+	def calculate_age(self, year):
+		age = 2015 - int(year)
+		if age > 10:
+			print("$500")
+		elif 10 > age > 5:
+			print("$1000")
+		else:
+			print("$2000")
+
+used_vehicle1 = Vehicle(parse_string(vehicle))
+used_vehicle1.calculate_age()
