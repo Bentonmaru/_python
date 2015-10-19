330283eed1382fbe63bf109514eb171a5fb4b483
Bentonmaru <js5m@mtmail.mtsu.edu>
2015-10-19 13:24:18 -0500

    Update vehicle.py

--- used_vehicles/vehicle.py
+++ used_vehicles/vehicle.py
@@ -1,3 +1,5 @@
+import time
+
 vehicle = raw_input("Please enter your brand, model, and year(e.g 1993) ")
 
 def parse_string(vehicle):
@@ -11,7 +13,7 @@ class Vehicle(object):
 		self.year = year
 
 	def calculate_age(self, year):
-		age = 2015 - int(year)
+		age = today.year - int(year)
 		if age > 10:
 			print("$500")
 		elif 10 > age > 5:
